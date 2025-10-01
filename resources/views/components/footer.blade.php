5bd8a393cffcbc3c01d517eccbdc614a17921ff5
liantarill <yusufherliananantaril@gmail.com>
2025-10-02 03:32:02 +0700

    Tugas Pertemuan 5 : Membuat navbar, footer, table dengan komponen.

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<style>
+    footer {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
+
+<footer class="bg-dark text-white text-center py-3 mt-auto">
+    <div class="container">
+        <small>&copy; {{ date('Y') }} Praktikum PWL. All rights reserved.</small>
+    </div>
+</footer>
